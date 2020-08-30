b43a4726c81decba3cc64061a983132f87292203
Sam Worrall <sam.worrall@yourgolftravel.com>
2020-08-30 13:55:19 +0100

    LogCounter counts unique page visits
    I would ideally like to avoid opening the file twice, so my plan is to
    refactor this class.

--- lib/log_counter.rb
+++ lib/log_counter.rb
@@ -4,15 +4,32 @@
 class LogCounter
   def initialize
     @total_page_visits = Hash.new(0)
+    @total_unique_page_visits = Hash.new(0)
   end
 
-  attr_reader :total_page_visits
+  attr_reader :total_page_visits, :total_unique_page_visits
 
   def aggregate_total_page_visits(filepath)
     File.open(filepath) do |file|
       file.readlines.each do |line|
         page_name = line.split(' ')[0]
         total_page_visits[page_name] += 1
+      end
+    end
+  end
+
+  def aggregate_total_unique_page_visits(filepath)
+    unique_logs = []
+
+    File.open(filepath) do |file|
+      file.readlines.each do |line|
+
+        if !unique_logs.include?(line.split(' '))
+          log = line.split(' ')
+          page_name = log[0]
+          total_unique_page_visits[page_name] += 1
+          unique_logs << log
+        end
       end
     end
   end
--- spec/log_counter_spec.rb
+++ spec/log_counter_spec.rb
@@ -5,9 +5,19 @@ require 'log_counter'
 RSpec.describe LogCounter do
   let(:filepath) { './spec/support/example.log' }
 
-  it 'Aggregates logs' do
+  it 'Aggregates logs and counts total page visits' do
     subject.aggregate_total_page_visits(filepath)
 
     expect(subject.total_page_visits['/example_page/1']).to eq(5)
+    expect(subject.total_page_visits['/example_page/2']).to eq(2)
+    expect(subject.total_page_visits['/example_page/3']).to eq(3)
+  end
+
+  it 'Aggregates logs and counts total unique page visits' do
+    subject.aggregate_total_unique_page_visits(filepath)
+
+    expect(subject.total_unique_page_visits['/example_page/1']).to eq(3)
+    expect(subject.total_unique_page_visits['/example_page/2']).to eq(2)
+    expect(subject.total_unique_page_visits['/example_page/3']).to eq(1)
   end
 end
